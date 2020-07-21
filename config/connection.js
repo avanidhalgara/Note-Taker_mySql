6472907d42737c33dd3c4cc5f1e67c1f3f15d743
unknown <avanidhalgara01@gmail.com>
2020-07-21 18:24:17 -0500

    updated databse

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql');
 const util = require('util');
-
-const connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL){
+  connection =mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'Krish2012$',
     database: 'Note_Taker'
   });
+}
+ 
 
 connection.connect(function(err) {
   if (err) {
